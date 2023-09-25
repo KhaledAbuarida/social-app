829b973f4160b6f1a27e2ccc6cb7e4c1bfd2fda7
khaled Abuarida <khaledabuaria0@gmail.com>
2023-09-25 21:25:38 +0300

    solving some responsive issues

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -6,32 +6,30 @@ import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 
 export default function Share() {
   return (
-    <div className='shareContainer' >
-        <div className="shareUpperPart">      
-            <img className='profileImg' src="/assets/person/1.jpeg" alt="profilePic" />
-            <input className='shareInput' placeholder="What's in your mind?" type="text"  />
-        </div>
-        <hr className='shareHr'/>
-        <div className="shareLowerPart">
-            <div className="shareOptions">
-                <div className="shareOption">
-                    <PermMediaIcon htmlColor='#28B463' className='shareOptionIcon'/>
-                    <span className="shareOptionText"> Photos </span>
-                </div>
-                <div className="shareOption">
-                    <LabelIcon htmlColor='#2471A3' className='shareOptionIcon'/>
-                    <span className='shareOptionText'>Tag</span>
-                </div>
-                <div className="shareOption">
-                    <LocationOnIcon htmlColor='#E74C3C' className='shareOptionIcon'/>
-                    <span className='shareOptionText'>Location</span>
-                </div>
-                <div className="shareOption">
-                    <EmojiEmotionsIcon htmlColor='#F1C40F' className='shareOptionIcon'/>
-                    <span className='shareOptionText'>Feelings</span>
-                </div>
-                <button className="shareBtn">Share</button>
+    <div className="share">
+        <div className='shareContainer' >
+            <div className="shareUpperPart">      
+                <img className='profileImg' src="/assets/person/1.jpeg" alt="profilePic" />
+                <input className='shareInput' placeholder="What's in your mind?" type="text"  />
+            </div>
+            <hr className='shareHr'/>
+            <div className="shareLowerPart">
+                <div className="shareOptions">
+                    <div className="shareOption">
+                        <PermMediaIcon htmlColor='#28B463' className='shareOptionIcon'/>
+                        <span className="shareOptionText"> Photos </span>
+                    </div>
+                    <div className="shareOption locationOption">
+                        <LocationOnIcon htmlColor='#E74C3C' className='shareOptionIcon'/>
+                        <span className='shareOptionText'>Location</span>
+                    </div>
+                    <div className="shareOption">
+                        <EmojiEmotionsIcon htmlColor='#F1C40F' className='shareOptionIcon'/>
+                        <span className='shareOptionText'>Feelings</span>
+                    </div>
+                    <button className="shareBtn">Share</button>
 
+                </div>
             </div>
         </div>
     </div>
